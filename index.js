e9e651a483f8a30238510764db6f14de6698b34e
Radthiek <44319561+Radthiek@users.noreply.github.com>
2018-11-20 08:10:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const fs = require("fs");
-const bot = new Discord.bot();
 var Jimp = require("jimp");
 const SQLite = require("better-sqlite3");
 const sql = new SQLite('./profile.sqlite');
